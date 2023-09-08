557217d2e32c8cf0dbf625275872f4eaf544857a
Sophia <chiaki.katsuno@live.ca>
2023-09-08 10:54:19 -0700

    function for breedFetcher

---
+++ lighthouse/api/breedFetcher.js
@@ -0,0 +1,18 @@
+const request = require("request");
+
+const breedName = process.argv[2];
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+request(url, (error, response, body) => {
+  if (error) {
+    console.error("Error: ", error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("Breed not found");
+  } else {
+    console.log(data[0].description);
+  }
+});
